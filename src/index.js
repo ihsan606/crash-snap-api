2be652360f8faa882aae4b07f5af710cedf58dc9
ihsansyafiul <ihsansyafiul@gmail.com>
2024-06-13 13:27:38 +0000

    Revert "feat: add model predict service"
    This reverts commit 0d79bcdce40a0c2daf5c08c043214998b0160784.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const authRoutes = require('./api/auth/auth.route');
-const predictRoutes = require('./api/predict/predict.route');
 
-const { loadObjectDetectionModel, loadRegressionModel } = require("./utils/loadModel");
 const app = express();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 3000;
@@ -20,26 +18,12 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-(async () => {
-  try {
-    const objectDetectionModel = await loadObjectDetectionModel();
-    const regressionModel = await loadRegressionModel();
 
-    app.use((req, res, next) => {
-      req.models = {
-        objectDetectionModel,
-        regressionModel
-      };
-      next();
-    });
 
-    app.use('/api/auth', authRoutes);
-    app.use('/api/predict', predictRoutes);
+app.use('/api/auth', authRoutes);
 
-    app.listen(port, () => {
-      console.log(`Listening on http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error('Failed to load models:', error);
-  }
-})();
+
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
